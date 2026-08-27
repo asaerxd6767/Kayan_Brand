9742a01b31efaa8407a25b1cdeafa3b7580e765b
asaerxd6767 <mohammedasser070@gmail.com>
2026-08-27 15:03:35 +0300

    feat: Implement product fetching from API and update HomeScreen to display products

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -16,6 +16,18 @@ class ProductModel {
     required this.description,
     required this.fabricCare,
   });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      name: json['title'],
+      image: json['images'][0],
+      category: json['category']['name'],
+      price: (json['price'] as num).toInt(),
+      description: json['description'],
+      fabricCare: 'Dry clean only. 100% Premium Silk.',
+    );
+  }
 }
 
 final List<ProductModel> dummyProducts = [
@@ -24,8 +36,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Obsidian Silk Trench',
     category: 'Outerwear',
     price: 895,
-    image: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1000&auto=format&fit=crop',
-    description: 'A classic obsidian silk trench coat crafted for modern luxury and timeless elegance.',
+    image:
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'A classic obsidian silk trench coat crafted for modern luxury and timeless elegance.',
     fabricCare: 'Dry clean only. 100% Premium Silk.',
   ),
   ProductModel(
@@ -33,8 +47,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Oversized Cotton Hoodie',
     category: 'Streetwear',
     price: 320,
-    image: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?q=80&w=1000&auto=format&fit=crop',
-    description: 'Heavyweight organic cotton hoodie with a relaxed, modern streetwear drop-shoulder fit.',
+    image:
+        'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Heavyweight organic cotton hoodie with a relaxed, modern streetwear drop-shoulder fit.',
     fabricCare: 'Machine wash cold with like colors. 100% Organic Cotton.',
   ),
   ProductModel(
@@ -42,8 +58,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Tailored Wool Trousers',
     category: 'Men',
     price: 450,
-    image: 'https://images.unsplash.com/photo-1617137984095-74e4e5e3613f?q=80&w=1000&auto=format&fit=crop',
-    description: 'Slim-fit formal wool trousers tailored with precise pleating for executive comfort.',
+    image:
+        'https://images.unsplash.com/photo-1617137984095-74e4e5e3613f?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Slim-fit formal wool trousers tailored with precise pleating for executive comfort.',
     fabricCare: 'Dry clean only. 90% Wool, 10% Cashmere.',
   ),
   ProductModel(
@@ -51,8 +69,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Minimalist Leather Bag',
     category: 'Accessories',
     price: 680,
-    image: 'https://images.unsplash.com/photo-1548036328-c9fa89d128fa?q=80&w=1000&auto=format&fit=crop',
-    description: 'Sleek everyday shoulder bag made from full-grain leather with brass hardware.',
+    image:
+        'https://images.unsplash.com/photo-1548036328-c9fa89d128fa?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Sleek everyday shoulder bag made from full-grain leather with brass hardware.',
     fabricCare: 'Wipe clean with a soft leather conditioner.',
   ),
   ProductModel(
@@ -60,8 +80,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Double-Breasted Blazer',
     category: 'Women',
     price: 790,
-    image: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1000&auto=format&fit=crop',
-    description: 'Sharp silhouette blazer featuring subtle shoulder pads and custom tortoiseshell buttons.',
+    image:
+        'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Sharp silhouette blazer featuring subtle shoulder pads and custom tortoiseshell buttons.',
     fabricCare: 'Dry clean only. Viscose blend.',
   ),
   ProductModel(
@@ -69,8 +91,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Cashmere Knit Sweater',
     category: 'Outerwear',
     price: 520,
-    image: 'https://images.unsplash.com/photo-1576995853123-5a10305d93c0?q=80&w=1000&auto=format&fit=crop',
-    description: 'Ultra-soft knitted crewneck sweater designed for lightweight warmth during cold seasons.',
+    image:
+        'https://images.unsplash.com/photo-1576995853123-5a10305d93c0?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Ultra-soft knitted crewneck sweater designed for lightweight warmth during cold seasons.',
     fabricCare: 'Hand wash cold or dry clean. 100% Mongolian Cashmere.',
   ),
   ProductModel(
@@ -78,8 +102,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Classic Denim Jacket',
     category: 'Streetwear',
     price: 280,
-    image: 'https://images.unsplash.com/photo-1543076447-215ad9ba6923?q=80&w=1000&auto=format&fit=crop',
-    description: 'Vintage washed denim jacket featuring reinforced stitching and custom metal buttons.',
+    image:
+        'https://images.unsplash.com/photo-1543076447-215ad9ba6923?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Vintage washed denim jacket featuring reinforced stitching and custom metal buttons.',
     fabricCare: 'Machine wash inside out. 100% Rigid Denim.',
   ),
   ProductModel(
@@ -87,8 +113,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Pleated Midi Skirt',
     category: 'Women',
     price: 360,
-    image: 'https://images.unsplash.com/photo-1583496661160-fb5886a0aaaa?q=80&w=1000&auto=format&fit=crop',
-    description: 'High-waisted knife-pleated midi skirt with a subtle satin shimmer effect.',
+    image:
+        'https://images.unsplash.com/photo-1583496661160-fb5886a0aaaa?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'High-waisted knife-pleated midi skirt with a subtle satin shimmer effect.',
     fabricCare: 'Gentle machine wash in mesh bag. 100% Polyester.',
   ),
   ProductModel(
@@ -96,8 +124,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Leather Chelsea Boots',
     category: 'Accessories',
     price: 490,
-    image: 'https://images.unsplash.com/photo-1608256246200-53e635b5b65f?q=80&w=1000&auto=format&fit=crop',
-    description: 'Handcrafted leather boots with elastic side panels and durable rubber soles.',
+    image:
+        'https://images.unsplash.com/photo-1608256246200-53e635b5b65f?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Handcrafted leather boots with elastic side panels and durable rubber soles.',
     fabricCare: 'Treat with leather wax periodically. Genuine Calfskin.',
   ),
   ProductModel(
@@ -105,8 +135,10 @@ final List<ProductModel> dummyProducts = [
     name: 'Structured Linen Shirt',
     category: 'Men',
     price: 210,
-    image: 'https://images.unsplash.com/photo-1598033129183-c4f50c736f10?q=80&w=1000&auto=format&fit=crop',
-    description: 'Breathable European linen shirt perfect for smart-casual summer layering.',
+    image:
+        'https://images.unsplash.com/photo-1598033129183-c4f50c736f10?q=80&w=1000&auto=format&fit=crop',
+    description:
+        'Breathable European linen shirt perfect for smart-casual summer layering.',
     fabricCare: 'Machine wash warm, hang dry. 100% Linen.',
   ),
 ];
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_brand/api/product_service.dart';
 import 'package:local_brand/core/theme/app_spacing.dart';
 import 'package:local_brand/widgets/category_card.dart';
 import 'package:local_brand/widgets/form_field.dart';
@@ -7,13 +8,17 @@ import '../core/routing/routes.dart';
 import '../models/product_model.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ApiService api = ApiService();
+  List<ProductModel> products = [];
+  bool isLoading = true;
+  final Set<int> favoriteProducts = <int>{};
   final List<String> categories = [
     'New Arrivals',
     'Men',
@@ -34,7 +39,20 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  final Set<int> favoriteProducts = <int>{};
+  void fetchProducts() async {
+    final result = await api.getProduct();
+    setState(() {
+      products = result;
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,34 +90,34 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
 
               // Cards
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: dummyProducts.length,
-                itemBuilder: (context, index) {
-                  final isFavorite = favoriteProducts.contains(index);
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(
-                        context,
-                        Routes.productDetails,
-                        arguments: 
-                        {
-                          'product': dummyProducts[index],
-                          'isFavorite': isFavorite,
-                          'onFavoriteTap': () => onFavoriteTap(index)
-                        }
-        
-                      );
-                    },
-                    child: CategoryCard(
-                      product: dummyProducts[index],
-                      onFavoriteTap: () => onFavoriteTap(index),
-                      isFavorite: isFavorite,
+              isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: dummyProducts.length,
+                      itemBuilder: (context, index) {
+                        final isFavorite = favoriteProducts.contains(index);
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(
+                              context,
+                              Routes.productDetails,
+                              arguments: {
+                                'product': products[index],
+                                'isFavorite': isFavorite,
+                                'onFavoriteTap': () => onFavoriteTap(index),
+                              },
+                            );
+                          },
+                          child: CategoryCard(
+                            product: products[index],
+                            onFavoriteTap: () => onFavoriteTap(index),
+                            isFavorite: isFavorite,
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             ],
           ),
         ),
