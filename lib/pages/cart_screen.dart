ad1c51d0864829a0ee81abca99088d0304349d2e
asaerxd6767 <mohammedasser070@gmail.com>
2026-08-31 18:39:22 +0300

    feat: Enhance CartScreen with product details navigation and favorite toggle functionality

--- lib/pages/cart_screen.dart
+++ lib/pages/cart_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:local_brand/core/theme/app_spacing.dart';
 import 'package:local_brand/managers/product_manager.dart';
+import 'package:local_brand/widgets/category_card.dart';
+
+import '../core/routing/routes.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -8,16 +11,51 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    final _manager = ProductManager.instance;
+    final manager = ProductManager.instance;
 
     return SingleChildScrollView(
       padding: EdgeInsets.all(KayanSpacing.md),
       child: Column(
         spacing: KayanSpacing.base,
         children: [
-          Text('Favorites', style: textTheme.headlineLarge,),
+          Text('Favorites', style: textTheme.headlineLarge),
 
-          
+          manager.cartProducts.isEmpty
+              ? Text(
+                  'The cart is empty, please add the products you want',
+                  textAlign: .center,
+                )
+              : ListenableBuilder(
+                  listenable: manager,
+                  builder: (context, _) => ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: manager.cartProducts.length,
+                    itemBuilder: (context, index) {
+                      final product = manager.cartProducts[index];
+                      final isFavorite = manager.isFavorite(product);
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            Routes.productDetails,
+                            arguments: {
+                              'product': product,
+                              'isFavorite': isFavorite,
+                              'onFavoriteTap': () =>
+                                  manager.toggleFavorite(product),
+                            },
+                          );
+                        },
+                        child: CategoryCard(
+                          product: product,
+                          onFavoriteTap: () => manager.toggleFavorite(product),
+                          isFavorite: isFavorite,
+                        ),
+                      );
+                    },
+                  ),
+                ),
         ],
       ),
     );
